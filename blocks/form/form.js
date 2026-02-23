74bded8afab92fb40bc1f231542628ff87a12ba3
tejaswiniindalkar24 <inf76768@adobe.com>
2026-02-23 22:45:14 +0530

    Added js for two function

--- blocks/form/form.js
+++ blocks/form/form.js
@@ -568,34 +568,3 @@ export default async function decorate(block) {
 }
 
 
-export function maskMobileNumber(mobileNumber) {
-if (!mobileNumber) {
-return '';
-}
-const value = mobileNumber.toString();
-// Mask first 5 digits and keep the rest
-return ` ${'*'.repeat(5)}${value.substring(5)}`;
-}
-
-
-
-export function startOtpTimer(duration = 60) {
-  let timeLeft = duration;
-
-  const timerDisplay = document.getElementById('otp-timer');
-  const resendBtn = document.getElementById('resend-btn');
-
-  resendBtn.disabled = true; 
-
-  const timer = setInterval(() => {
-    timerDisplay.textContent = `Resend OTP in: ${timeLeft} secs`;
-
-    if (timeLeft <= 0) {
-      clearInterval(timer);
-      timerDisplay.textContent = "You can resend OTP now";
-      resendBtn.disabled = false;
-    }
-
-    timeLeft--;
-  }, 1000);
-}
